f3c3aefc394a65783124bfe2df875191f07b2782
Nick Hanson <hansonnick25@gmail.com>
2023-09-21 19:05:05 -0500

    Adding in date to entries

--- models/Entries.js
+++ models/Entries.js
@@ -19,6 +19,10 @@ Entries.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
     actualSets: {
       type: DataTypes.INTEGER,
       allowNull: true,
