141af3e3ffa7d7645fbf17c1f88befe7cee28840
Nick Hanson <hansonnick25@gmail.com>
2023-09-21 20:58:21 -0500

    Adding in template and entry views

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -4,39 +4,37 @@ const withAuth = require('../../utils/auth');
 
 // CREATE NEW TEMPLATE
 router.post('/', withAuth, async (req, res) => {
-    try {
-      const newEntry = await Entry.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newEntry);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
+  try {
+    const newEntry = await Entry.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newEntry);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 // DELETE TEMPLATE
-
-
 router.delete('/:id', withAuth, async (req, res) => {
-    try {
-      const entryData = await Entry.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (!entryData) {
-        res.status(404).json({ message: 'No entry found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(entryData);
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    const entryData = await Entry.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!entryData) {
+      res.status(404).json({ message: 'No entry found with this id!' });
+      return;
     }
-  });
-  
-  module.exports = router;
+
+    res.status(200).json(entryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- models/User.js
+++ models/User.js
@@ -38,12 +38,15 @@ User.init(
   },
   {
     hooks: {
-      beforeCreate: async (newUserData) => {
+      beforeCreate: async newUserData => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+      beforeUpdate: async updatedUserData => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
         return updatedUserData;
       },
     },
